76daaeda945a1fdc9ec2bb0e6311e431a710fec4
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-05-08 21:30:45 -0500

    May 8 2023: Test to see if recipe.json was successful. Still updating json; ready to add more event listeners

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("javascript file connected");
+
+function recipetest() {
+        fetch ('http://localhost:3000/Recipes')
+        .then(resp => resp.json())
+        .then(json =>console.log(json))
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    recipetest()
+});
