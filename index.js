2549c2d54ce28dfd23fabbda3c1fc4904926dd03
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-05-14 08:25:49 -0500

    May 14 2023 Test for function recipeSection. Attempting to populate on console.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.log("javascript file connected");
 
+document.addEventListener('DOMContentLoaded', function(){
+    recipeSection(listRecipe);
+});
+
+function recipeSection (listRecipe){
+    document.createElement(listRecipe);
+}
+
 function recipetest() {
         fetch ('http://localhost:3000/Recipes')
         .then(resp => resp.json())
         .then(json =>console.log(json))
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-    recipetest()
-});
